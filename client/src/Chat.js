04058d0bdedef64ea307aad6846cdf5a263bcc98
Shirley Fichman <shirleyfichman@gmail.com>
2024-09-08 15:20:54 +0300

    showing sent msgs

--- client/src/Chat.js
+++ client/src/Chat.js
@@ -3,6 +3,7 @@ import { useEffect, useState } from 'react';
 
 function Chat({socket, userName, room}) {
     const [currentMsg, setCurrentMsg] = useState("")
+    const [msgList, setMsgList] = useState([])
 
     const sendMsg = async () => {
         if (currentMsg !== "") {
@@ -15,30 +16,45 @@ function Chat({socket, userName, room}) {
                 new Date(Date.now()).getMinutes(),
             };
             await socket.emit("sendMsg", msgData);
+            setMsgList((list) => [...list, msgData]);
         }
     }
 
     useEffect(() => {
         socket.on("receiveMsg", (data) => {
-            console.log(data);
+            setMsgList((list) => [...list, data]);
         });
     }, [socket]);
 
     return(<>
-    <div className='chat-header'>
-        <p>Live Chat</p>
+    <div className='chat-window'>
+        <div className='chat-header'>
+            <p>Live Chat</p>
 
-    </div>
-    <div className='chat-body'>
+        </div>
+        <div className='chat-body'>
+            {msgList.map((msgContent) => {
 
+                <div className='message'>
+                    <div>
+                        <div className='message-content'>
+                            <p>{msgContent.msg}</p>
+                        </div>
+                        <div className='message-meta'>
+                            <p>{msgContent.time}</p>
+                            <p>{msgContent.author}</p>
+                        </div>
+                    </div>
+                </div>
+            })}
+        </div>
+        <div className='chat-footer'>
+            <input type="text" 
+            onChange={(e) => {setCurrentMsg(e.target.value);}}>
+            </input>
+            <button onClick={sendMsg}>Send</button>
+        </div>
     </div>
-    <div className='chat-footer'>
-        <input type="text" 
-        onChange={(e) => {setCurrentMsg(e.target.value);}}>
-        </input>
-        <button onClick={sendMsg}>Send</button>
-    </div>
-
     </>)
 }
 
